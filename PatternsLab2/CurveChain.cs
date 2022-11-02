f9c821f165880e721ecb850d0cd857206155cbb7
Rastuharem <Rastuharem@yandex.com>
2022-11-02 19:53:21 +0300

    Lab #5 finished

--- PatternsLab2/CurveChain.cs
+++ PatternsLab2/CurveChain.cs
@@ -6,11 +6,27 @@ namespace PatternsLab2
     {
         private List<ICurve> ChainedCurves;
         private int CurvesCount;
+        List<FragmentDecorator> Fragments;
 
         public CurveChain(List<ICurve> curves)
         {
             this.ChainedCurves = curves;
             this.CurvesCount = curves.Count;
+
+            Fragments = new List<FragmentDecorator>();
+            double chainDivider = 1 / (double)CurvesCount;
+            for (int i = 1; i < CurvesCount + 1; i++)
+            {
+                Fragments.Add(new FragmentDecorator(ChainedCurves[i - 1], chainDivider * (i - 1), chainDivider * i));
+            }
+
+            for (int i = 1; i < CurvesCount; i++)
+            {
+                Fragments[i - 1].GetPoint(1, out IPoint endPoint);
+                FragmentDecorator buf = new FragmentDecorator(new MoveToDecorator(Fragments[i], endPoint), Fragments[i].tmin, Fragments[i].tmax);
+                Fragments[i] = buf;
+            }
+
         }
 
         public ICurve GetComponent()
@@ -19,24 +35,15 @@ namespace PatternsLab2
         }
         public void GetPoint(double t, out IPoint p)
         {
-            List<FragmentDecorator> Fragments = new List<FragmentDecorator>();
             double chainDivider = 1 / (double)CurvesCount;
             int chainedCurveNumber = 0;
 
             for (int i = 1; i < CurvesCount + 1; i++)
             {
-                Fragments.Add(new FragmentDecorator(ChainedCurves[i - 1], chainDivider * (i - 1), chainDivider * i));
                 if (t > chainDivider * (i - 1) && t <= chainDivider * i)
                 {
                     chainedCurveNumber = i - 1;
                 }
-            }
-
-            for (int i = 1; i < CurvesCount; i++)
-            {
-                Fragments[i - 1].GetPoint(1, out IPoint endPoint);
-                FragmentDecorator buf = new FragmentDecorator(new MoveToDecorator(Fragments[i], endPoint), Fragments[i].tmin, Fragments[i].tmax);
-                Fragments[i] = buf;
             }
 
             double curt = (t - Fragments[chainedCurveNumber].tmin) / (Fragments[chainedCurveNumber].tmax - Fragments[chainedCurveNumber].tmin);
